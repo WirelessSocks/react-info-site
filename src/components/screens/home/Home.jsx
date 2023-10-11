7125ea83bb7054bbbc01402d28a5f6fd13bd1013
WirelessSocks <romka116rus@icloud.com>
2023-10-11 22:44:22 +0300

    Created react interesting info site

---
+++ src/components/screens/home/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <img alt='react-icon' src='./assets/logo192.png' width="40px"/>
+            <h1>Fun facts about React</h1>
+            <ul>
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100k stars on Github</li>
+                <li>is maintained by Faceook</li>
+                <li>Powers thousands of enterprise apps, incuding mobile apps</li>
+            </ul>
+        </div>
+    )
+        
+};
+
+export default Home;
+
